3362c3728b6ff1ba1f40df9debcc640b37339600
amirkenesbay <amirkenesbay@gmail.com>
2022-01-27 00:55:16 +0600

    Add exception handler

--- src/main/java/com/amirkenesbay/spring/rest/controller/MyRestController.java
+++ src/main/java/com/amirkenesbay/spring/rest/controller/MyRestController.java
@@ -1,12 +1,13 @@
 package com.amirkenesbay.spring.rest.controller;
 
 import com.amirkenesbay.spring.rest.entity.Employee;
+import com.amirkenesbay.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.amirkenesbay.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.amirkenesbay.spring.rest.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +26,23 @@ public class MyRestController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable int id){
         Employee employee = employeeService.getEmployee(id);
+        if(employee == null){
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " in Database");
+        }
         return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
