350f2d0fb3ea84ead31fd9e4618556546f0b5019
amirkenesbay <amirkenesbay@gmail.com>
2022-01-31 22:22:06 +0600

    Add global exception handler

--- src/main/java/com/amirkenesbay/spring/rest/controller/MyRestController.java
+++ src/main/java/com/amirkenesbay/spring/rest/controller/MyRestController.java
@@ -31,18 +31,4 @@ public class MyRestController {
         }
         return employee;
     }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployeeException exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
 }
